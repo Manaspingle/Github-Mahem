d0c8d9b8d4c4c71503de8e0cd55709ed0514b4e1
Manas Pingle <manaspingle6174@gmail.com>
2025-08-03 11:50:43 +0530

    Java Calculator feature

---
+++ First.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class First {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Welcome to Calculator");
+    int a , b;
+    System.out.println("Enter two numbers: ");
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
+    
+
+  }
+  
+}
